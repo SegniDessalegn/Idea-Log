a3f9ae002502267ec56ec98e0419faf287fcf442
Segni Dessalegn <segnides@gmail.com>
2023-08-14 23:11:31 +0300

    🎉 initialize project

---
+++ plugins/firebase.client.js
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from "firebase/auth"
+import { getFirestore } from 'firebase/firestore'
+
+export default defineNuxtPlugin(nuxtApp => {
+    const config = useRuntimeConfig().public
+
+    const firebaseConfig = {
+        apiKey: config.FIREBASE_API_KEY,
+        authDomain: config.FIREBASE_AUTH_DOMAIN,
+        projectId: config.FIREBASE_PROJECT_ID,
+        storageBucket: config.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID,
+        appId: config.FIREBASE_APP_ID,
+    };
+
+    const app = initializeApp(firebaseConfig)
+
+    const auth = getAuth(app)
+    const firestore = getFirestore(app)
+
+    nuxtApp.vueApp.provide('auth', auth)
+    nuxtApp.provide('auth', auth)
+
+    nuxtApp.vueApp.provide('firestore', firestore)
+    nuxtApp.provide('firestore', firestore)
+})
