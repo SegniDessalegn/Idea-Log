fb4935831ce44a6916e7b2f4e21be58ac34224ad
Segni Dessalegn <segnides@gmail.com>
2023-08-15 16:06:51 +0300

    🎨 add login page design

---
+++ pages/login.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <div class="flex flex-col bg-gray-100 p-10 mt-20 rounded-lg items-center w-fit mx-auto gap-5">
+        <div class="text-lg">Welcome, <b>Login</b> to Idea Log</div>
+        <input type="text" placeholder="Email" class="input input-bordered input-primary w-full max-w-xs" />
+        <input type="text" placeholder="Password" class="input input-bordered input-primary w-full max-w-xs" />
+        <div class="flex gap-5">
+            <div>Remember me for a month</div>
+            <input type="checkbox" class="toggle toggle-primary" checked />
+        </div>
+        <span class="loading loading-ring loading-lg"></span>
+        <button class="btn btn-primary w-full max-w-xs">Log in</button>
+        <div>If you do not have an account, you can <a class="link link-hover"><b>Signup</b></a>
+        </div>
+    </div>
+</template>
