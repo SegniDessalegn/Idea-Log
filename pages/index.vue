735d419712039b7aa6e1fb39809d21d86f8be20e
Segni Dessalegn <segnides@gmail.com>
2023-08-16 06:38:06 +0300

    update home page

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,10 @@ async function handleRegistration() {
   await registerUser(creds.email, creds.password)
 }
 </script>
-
 <template>
-    <div class="text-blue-500">Welcome</div>
+  <div class="flex flex-col items-center justify-center min-h-screen">
+    <h1 class="text-4xl font-bold mb-4">Welcome to Idea Log</h1>
+    <p class="text-lg">🚧Under development🚧</p>
+  </div>
 </template>
+
