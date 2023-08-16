ed2aebfb97d0190a41ba47a5373f353c3f6a918d
Segni Dessalegn <segnides@gmail.com>
2023-08-16 15:55:54 +0300

    add ideas list page

---
+++ components/Card/CardIdea.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="card w-96 hover:shadow-xl transition duration-500 cursor-pointer m-3 border-[1px] border-gray">
+        <div class="card-body">
+            <h2 class="card-title">Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore laborum aut natus quisquam itaque modi maiores omnis aspernatur ullam fugiat. Dolores fuga vero debitis ab?</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-sm btn-primary">Done</button>
+                <button class="btn btn-sm btn-error">Delete</button>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ pages/Ideas.vue
@@ -0,0 +1,38 @@
+
+<template>
+    <div class="text-3xl font-bold p-5 m-5">These are your idea logs</div>
+    <div class="divider m-0"></div>
+    <div class="tabs tabs-boxed bg-white pl-10 my-5 flex justify-between items-center nd w-full">
+        <div class="flex">
+            <a class="tab tab-active">Not Done</a>
+            <a class="tab">Done</a>
+        </div>
+        <div class="btn btn-primary mx-10">Add New</div>
+    </div>
+    <div class="flex justify-center flex-wrap">
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+        <CardIdea />
+    </div>
+</template>
