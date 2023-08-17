033a15bbb3ed3ca3236f0fc1d4ea1386e8efbe47
Segni Dessalegn <segnides@gmail.com>
2023-08-18 00:35:06 +0300

    add ideas button

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,10 @@ const { getCurrentUserLocalStorage } = useFirebaseAuth()
   <div class="flex flex-col items-center justify-center min-h-screen">
     <h1 class="text-4xl font-bold mb-4">Welcome to Idea Log</h1>
     <p class="text-lg">🚧Under development🚧</p>
-    <nuxt-link to="/signup" class="btn btn-primary m-5">Signup early</nuxt-link>
+    <div class="flex">
+      <nuxt-link to="/ideas" class="btn btn-neutral m-5">Explore Ideas</nuxt-link>
+      <nuxt-link to="/signup" class="btn btn-primary m-5">Signup early</nuxt-link>
+    </div>
   </div>
 </template>
 
